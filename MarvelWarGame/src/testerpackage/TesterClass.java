55b1c0c31b7c3272cb1ed1bae9be6b863009a9c0
lenovo <lenovo@DESKTOP-PHGVLVR>
2022-03-21 21:44:44 +0200

    First trial

--- MarvelWarGame/src/testerpackage/TesterClass.java
+++ MarvelWarGame/src/testerpackage/TesterClass.java
@@ -4,5 +4,6 @@ public class TesterClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello bros!");
+		System.out.print("elensan da3eef");
 	}
 }
